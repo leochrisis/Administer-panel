6a0c24d1095a9a136a71dcda088aa981431d2af8
leochrisis <leonardo.chfc@gmail.com>
2017-07-13 11:00:29 -0300

    Start administrator validation

---
+++ src/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="1">
+        Administer validation
+      </q-toolbar-title>
+    </div>
+
+    <div class="layout-view">
+      <div class="teste">
+        <button class="primary" v-link="'/panel'">
+          <i>whatshot</i>
+          Begin tests
+        </button>
+      </div>
+    </div>
+  </q-layout>
+</template>
+
+<style lang="sass">
+  .teste
+    margin: 20% auto
+    text-align: center
+</style>
