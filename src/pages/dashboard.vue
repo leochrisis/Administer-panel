5c4354feb48d0664ceaa62ecd49c20c132187fb8
leochrisis <leonardo.chfc@gmail.com>
2017-07-13 11:04:22 -0300

    Start app dashboard

---
+++ src/pages/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      Here go the statistic informations
+  </div>
+</template>
+
+<script>
+
+  export default{
+    name: 'Dashboard',
+
+    data: () => ({
+      admin: {
+        email: 'leonardo.chfc@gmail.com',
+        name: 'leochrisis',
+        display_name: '@leochrisis'
+      }
+    })
+  }
+</script>
